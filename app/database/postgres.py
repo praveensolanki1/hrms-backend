0e067034c105ea3815534bc5bfa538630a5f61c6
praveen solanki <psolanki6503@gmail.com>
2026-03-10 12:02:52 +0530

    Use environment variable for Postgres URL

--- app/database/postgres.py
+++ app/database/postgres.py
@@ -3,7 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/hrms"
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
